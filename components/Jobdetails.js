ee68f784009a9261bc821dea9537929bbfe24836
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-12-07 11:28:36 +0300

    Add some element for the job filter and style some of the element

--- components/Jobdetails.js
+++ components/Jobdetails.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const JobDetailStyle = styled.section`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 10px;
+    background-color: #fff;
+    padding: 12px;
+    border-radius: 4px;
+
+    img {
+        width: 90px;
+        border-radius: 4px;
+    }
+`;
 
 function Jobdetails({ job }) {
     return (
-        <div>
-            <p>{job.location}</p>
-        </div>
+        <JobDetailStyle>
+            <h2>
+                <img src={job.company_logo} alt={`${job.title} at ${job.company}`} />
+            </h2>
+            <div>
+                <span>{job.company}</span>
+                <p>{job.title}</p>
+                <span>{job.type}</span>
+            </div>
+        </JobDetailStyle>
     )
 }
 
--- pages/Jobs.js
+++ pages/Jobs.js
@@ -1,6 +1,13 @@
 import React, { useContext } from 'react'
+import styled from 'styled-components';
 import JobDetails from "../components/Jobdetails"
 import { GlobalContext } from './GlobalContext';
+
+const JobsStyle = styled.article`
+    display: grid;
+    gap: 20px;
+    // grid-template-rows: 1fr 2fr;
+`;
 
 function Jobs() {
     const { state } = useContext(GlobalContext);
@@ -9,7 +16,9 @@ function Jobs() {
     return (
         <div>
             {loading && <p>Loading...</p>}
-            {githubJobs.map(job => <JobDetails key={job.id} job={job} />)}
+            <JobsStyle>
+                {githubJobs.map(job => <JobDetails key={job.id} job={job} />)}
+            </JobsStyle>
         </div>
     )
 }
