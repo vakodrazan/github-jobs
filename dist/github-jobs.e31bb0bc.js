9e2736b7ad0a1b3f336566d0d740e8bbd5fb5ee3
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-01-28 16:58:53 +0300

    Create the basic folders and files that are needed

--- dist/github-jobs.e31bb0bc.js
+++ dist/github-jobs.e31bb0bc.js
@@ -36549,7 +36549,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57332" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52244" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
